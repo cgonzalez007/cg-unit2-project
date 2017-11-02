7cb233e57388f926d5a8bda4e8710e8e4fe2378a
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-11-02 09:37:55 -0500

    commit #10: -small fixes -further defined finalize html page and interactions

--- public_html/js/finalize.js
+++ public_html/js/finalize.js
@@ -1,7 +1,33 @@
 $(function () {
+
     $('#continue-guest-btn').on('click', function () {
-        $('#login-register-step').fadeOut(1000);
-        $('#order-info-step').fadeIn(3000);
+        $('#login-register-step').fadeOut(1000, function () {
+            $('#order-info-step').fadeIn(3000);
+        });
+
     });
+
+    $('#continue-to-last-step-btn').on('click', function () {
+        $('#order-info-step').fadeOut(1000, function () {
+            $('#last-step').fadeIn(3000);
+        });
+
+    });
+
+    /*Clicks to go back to previous steps*/
+    $('#back-to-login-register-anchor').on('click', function () {
+        $('#order-info-step').fadeOut(1000, function () {
+            $('#login-register-step').fadeIn(3000);
+        });
+
+    });
+
+    $('#back-to-order-info-anchor').on('click', function () {
+        $('#last-step').fadeOut(1000, function () {
+            $('#order-info-step').fadeIn(3000);
+        });
+
+    });
+
 });
 
