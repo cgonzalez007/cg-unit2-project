dd7ae11274f742428a329129da0f838debcfebb0
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-11-01 20:45:38 -0500

    commit #8: -fixed bugs with header nav bar -made header nav bar much more responsive -documented and organized script and link tags

---
+++ public_html/js/index.js
@@ -0,0 +1,16 @@
+$(function () {
+    initFirebase();
+});
+
+function initFirebase() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDrbbjK0tYP8IvpjoM3pCOHMvtFV_Gnm3s",
+        authDomain: "cg-unit2-project.firebaseapp.com",
+        databaseURL: "https://cg-unit2-project.firebaseio.com",
+        projectId: "cg-unit2-project",
+        storageBucket: "cg-unit2-project.appspot.com",
+        messagingSenderId: "302302304461"
+    };
+    firebase.initializeApp(config);
+}
